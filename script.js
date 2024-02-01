346ff5c05ced3b8a10ff9395cf81c414279beed7
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-02-01 10:52:02 -0600

    Exercise completed.

---
+++ script.js
@@ -0,0 +1,23 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs.toUpperCase())
+console.log(warmHugs.replace("like", "love"))
+
+beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(17, 36))
+
+dotDotDot = "..."
+skullAndBones = `I don't have a skull${dotDotDot}or bones`
+console.log(skullAndBones)
+
+console.log(Math.PI)
+
+randomNumber = Math.ceil(Math.random(1, 3))
+console.log(randomNumber)
+// BONUS
+let letItGo = " Let it go!"
+console.log(letItGo.toUpperCase().repeat(2))
+
+reindeers = "Reindeers are better than people."
+reindeers = reindeers.replace(" ", "_")
+console.log(reindeers)
+
